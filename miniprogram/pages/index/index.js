6cdf23c3bb87f84bffa8d87137f65b2f02f6a927
TwilightLogic <twilightlogic@gmail.com>
2023-07-26 13:43:03 +0800

    Re-initializing the project

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,12 +1,69 @@
-// index.js
-// const app = getApp()
-const { envList } = require('../../envList.js');
+const app = getApp();
+const db = wx.cloud.database();
 
 Page({
-  data: {},
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiper: [],
+  },
 
-  // 搜索事件
+  // 搜索框事件
   onSearch(e) {
     console.log(e.detail);
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    let that = this;
+    db.collection('swiper').get({
+      success: (res) => {
+        console.log('轮播图获取成功', res);
+        that.setData({
+          swiper: res.data,
+        });
+      },
+      fail: (err) => {
+        console.log('轮播图获取失败', err);
+      },
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
 });
