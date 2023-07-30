21f6c9708434a76697d908cd7ad9f105c31e9721
TwilightLogic <twilightlogic@gmail.com>
2023-07-30 18:18:52 +0800

    Building project #24

--- miniprogram/pages/product_detail/product_detail.js
+++ miniprogram/pages/product_detail/product_detail.js
@@ -9,7 +9,6 @@ Page({
   data: {
     product: {},
     select_num: 1,
-    select_specs: '',
   },
 
   // 立即购买
@@ -25,7 +24,7 @@ Page({
     } else {
       let goods_list = {
         product_id: product._id,
-        product_img: product.img[0],
+        product_img: product.img_swiper[0],
         product_name: product.name,
         product_price: product.price,
         product_specs: that.data.select_specs,
@@ -64,60 +63,50 @@ Page({
     let that = this;
     let product = that.data.product;
 
-    // 如果没有选择规格的话，要提示用户选择规格
-    if (that.data.select_specs == '') {
-      wx.showToast({
-        title: '请选择规格',
-        icon: 'none',
-      });
-    } else {
-      wx.showLoading({
-        title: '添加中...',
-      });
-      // 这里做了一个检查用户是否重复添加商品的操作
-      db.collection('shopping_cart')
-        .where({
-          product_id: product._id,
-          product_specs: that.data.select_specs,
-        })
-        .get()
-        .then((res) => {
-          if (res.data.length > 0) {
-            wx.hideLoading();
-            wx.showToast({
-              title: '商品已存在',
-              icon: 'none',
-            });
-          } else {
-            db.collection('shopping_cart')
-              .add({
-                data: {
-                  product_id: product._id,
-                  product_img: product.img[0],
-                  product_name: product.name,
-                  product_price: product.price,
-                  product_specs: that.data.select_specs,
-                  product_num: that.data.select_num,
-                  time: db.serverDate(),
-                },
-              })
-              .then((res) => {
-                wx.hideLoading();
-                wx.showToast({
-                  title: '添加成功',
-                });
-                console.log('添加购物车', res);
+    wx.showLoading({
+      title: '添加中',
+    });
+    // 这里做了一个检查用户是否重复添加商品的操作
+    db.collection('shopping_cart')
+      .where({
+        product_id: product._id,
+      })
+      .get()
+      .then((res) => {
+        if (res.data.length > 0) {
+          wx.hideLoading();
+          wx.showToast({
+            title: '商品已存在',
+            icon: 'none',
+          });
+        } else {
+          db.collection('shopping_cart')
+            .add({
+              data: {
+                product_id: product._id,
+                product_img: product.img_swiper[0],
+                product_name: product.name,
+                product_price: product.price,
+                product_num: that.data.select_num,
+                time: db.serverDate(),
+              },
+            })
+            .then((res) => {
+              wx.hideLoading();
+              wx.showToast({
+                title: '添加成功',
               });
-          }
-        });
-    }
+              console.log('添加购物车', res);
+            });
+        }
+      });
   },
 
   // 获取商品详情信息
   get_product(id) {
     let that = this;
     wx.showLoading({
-      title: 'Loading...',
+      title: '加载中',
     });
     db.collection('products')
       .doc(id)
