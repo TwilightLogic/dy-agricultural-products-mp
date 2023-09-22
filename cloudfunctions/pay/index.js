860a0ae2f032d8d2d398c0362a64211f11ebea15
TwilightLogic <twilightlogic@gmail.com>
2023-09-22 13:48:29 +0800

    Deploy pay cloud function

--- cloudfunctions/pay/index.js
+++ cloudfunctions/pay/index.js
@@ -1,16 +1,39 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
+// 云函数代码
+const cloud = require('wx-server-sdk');
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+});
 
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
-
-// 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  const res = await cloud.cloudPay.unifiedOrder({
+    body: '广州康盛农业科技',
+    outTradeNo: '121775' + new Date().getTime(),
+    spbillCreateIp: '127.0.0.1',
+    subMchId: '1654142664',
+    totalFee: 1,
+    envId: 'cloud1-9g7ndjtrbeb3a024',
+    functionName: 'pay_cb',
+  });
+  return res;
+};
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
-}
+// 小程序代码
+wx.cloud.callFunction({
+  name: '函数名',
+  data: {
+    // ...
+  },
+  success: (res) => {
+    const payment = res.result.payment;
+    wx.requestPayment({
+      ...payment,
+      success(res) {
+        console.log('pay success', res);
+      },
+      fail(err) {
+        console.error('pay fail', err);
+      },
+    });
+  },
+  fail: console.error,
+});
